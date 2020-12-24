1136ffc24e9f1d2ff0a52d90c14861dcff6b55ac
Awis Qirani <qiraniawis@gmail.com>
2020-12-25 03:18:59 +0800

    Fix error in 6.2 Exercise 1 CLIENT

--- 6.2.Exercise-1-CLIENT.py
+++ 6.2.Exercise-1-CLIENT.py
@@ -16,7 +16,7 @@ print(Response)
 
 while True:
 	Input = input('Say Something: ')
-	ClientSocket.send(str.encode(input))
+	ClientSocket.send(str.encode(Input))
 	Response = ClientSocket.recv(1024)
 	print(Response.decode('utf-8'))
 
