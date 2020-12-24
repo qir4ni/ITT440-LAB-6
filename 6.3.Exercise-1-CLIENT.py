cdb827b9602fe94366d4503592c3b8cd1f513cb4
qir4ni <qiraniawis@gmail.com>
2020-12-25 06:04:19 +0800

    Fix error: Option for client's operation

--- 6.3.Exercise-1-CLIENT.py
+++ 6.3.Exercise-1-CLIENT.py
@@ -15,7 +15,28 @@ Response =ClientSocket.recv(1024)
 print(Response)
 
 while True:
-	Input = input('Enter type of operation with first and second value (Example: minus 1 1: ')
+
+	statu = 0
+	while(status == 0):
+		option = input('Enter type of operation (log | sqrt | exp): ')
+		if(option == 'log'):
+			value1 = input("Enter first value: ")
+			value2 - input("Enter second value for base: ")
+			status = 1
+		elif(option == 'sqrt'):
+			value1 = input("Enter the value: ")
+			value2 = '0'
+			status = 1
+		elif(option == 'exp'):
+			value1 = input("Enter the value: ")
+			value2 == '0'
+			status = 1
+		else:
+			print("Invalid operation, please re-enter..")
+			status = 0
+
+
+	Input = option + value1 + value2
 	ClientSocket.send(str.encode(Input))
 	Response = ClientSocket.recv(1024)
 	print(Response.decode('utf-8'))
