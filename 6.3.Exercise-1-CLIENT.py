b806fe63302dc58e28fb6719eaa03bd4dde0cf02
qir4ni <qiraniawis@gmail.com>
2020-12-25 04:58:32 +0800

    Adding input for client

--- 6.3.Exercise-1-CLIENT.py
+++ 6.3.Exercise-1-CLIENT.py
@@ -15,7 +15,7 @@ Response =ClientSocket.recv(1024)
 print(Response)
 
 while True:
-	Input = input('Say Something: ')
+	Input = input('Enter type of operation with first and second value (Example: minus 1 1: ')
 	ClientSocket.send(str.encode(Input))
 	Response = ClientSocket.recv(1024)
 	print(Response.decode('utf-8'))
